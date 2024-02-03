e99b412646963c5845ca890f4838ce6055345bae
Nick <whiteviperx@gmail.com>
2024-02-02 16:53:13 -0800

    Part_22_Middle
    Saving & Loading #1 mid point 28:42min

---
+++ Assets/Scripts/MainMenuSaveManager.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+public class MainMenuSaveManager:MonoBehaviour
+	{
+	public static MainMenuSaveManager Instance { get; set; }
+
+	private void Awake()
+		{
+		if (Instance != null && Instance != this)
+			{
+			Destroy (gameObject);
+			}
+		else
+			{
+			Instance = this;
+			}
+		}
+
+	[System.Serializable]
+	public class VolumeSettings
+		{
+		public float music;
+
+		public float effects;
+
+		public float master;
+		}
+
+	public void SaveVolumeSettings(float _music, float _effects, float _master)
+		{
+		VolumeSettings volumeSettings = new ()
+			{
+			music = _music,
+			effects = _effects,
+			master = _master
+			};
+
+		PlayerPrefs.SetString ("Volume", JsonUtility.ToJson (volumeSettings));
+		PlayerPrefs.Save ();
+		}
+
+	public VolumeSettings LoadVolumeSettings()
+		{
+		return JsonUtility.FromJson<VolumeSettings> (PlayerPrefs.GetString ("Volume"));
+		}
+	}
