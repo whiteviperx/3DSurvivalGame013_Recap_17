5db52bf08722bf2477fe7cc9a5c13b879490e01d
Nick <whiteviperx@gmail.com>
2024-03-14 22:08:44 -0700

    Part_32_Start

--- Assets/Scripts/Animal.cs
+++ Assets/Scripts/Animal.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+
 using UnityEngine;
 
 public class Animal:MonoBehaviour
@@ -17,36 +19,99 @@ public class Animal:MonoBehaviour
 
 	[SerializeField] private AudioClip rabbitHitAndDie;
 
+	private Animator animator;
+	public bool isDead;
+
+	[SerializeField] ParticleSystem bloodSplashParticles;
+	[SerializeField] GameObject bloodPuddle;
+
+
+	private enum AnimalType
+	{
+		Rabbit,
+
+		Lion,
+
+		Snake
+	}
+
+	[SerializeField] AnimalType thisAnimalType;
+
 	private void Start()
 	{
 		currentHealth = maxHealth;
+
+		animator = GetComponent<Animator>();
 	}
 
 	public void TakeDamage(int damage)
 	{
-		currentHealth -= damage;
+		if(isDead == false)
+		{
+			currentHealth -= damage;
 
-		if(currentHealth <= 0)
-		{
-			soundChannel.PlayOneShot(rabbitHitAndDie);
-			Destroy(gameObject);
-		}
-		else
-		{
-			soundChannel.PlayOneShot(rabbitHitAndScream);
+			bloodSplashParticles.Play();
+
+
+			if(currentHealth <= 0)
+			{
+				PlayDyingSound();
+
+				animator.SetTrigger("Die");
+				GetComponent<AI_Movement>().enabled = false;
+
+				StartCoroutine(PuddleDelay());
+
+				isDead = true;
+			}
+			else
+			{
+				PlayHitSound();
+			} 
 		}
 	}
 
-	//private void PlayDyingSound()
-	//{
+	IEnumerator PuddleDelay()
+	{
+		yield return new WaitForSeconds(1f);
+		bloodPuddle.SetActive(true);
+	}
 
-	//}
+	private void PlayDyingSound()
+	{
+		switch(thisAnimalType)
+		{
+			case AnimalType.Rabbit:
+				soundChannel.PlayOneShot(rabbitHitAndDie);
+				break;
+			case AnimalType.Lion:
 
-	//private void PlayHitSound()
-	//{
+				// soundChannel.PlayOneShot(); // Lion Sound Clip
+				break;
+			default:
+				break;
 
-	//}
+				// soundChannel.PlayOneShot(rabbitHitAndDie);
+		}
+	}
 
+	private void PlayHitSound()
+	{
+		switch(thisAnimalType)
+		{
+			case AnimalType.Rabbit:
+				soundChannel.PlayOneShot(rabbitHitAndScream);
+				break;
+			case AnimalType.Lion:
+
+				// soundChannel.PlayOneShot(); // Lion Scream Sound Clip
+				break;
+			default:
+				break;
+
+				// soundChannel.PlayOneShot(rabbitHitAndDie);
+		}
+	}
 
 	// --- Is player in range of animal? --- //
 	private void OnTriggerEnter(Collider other)
